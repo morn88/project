df1b1d175e86ded072b97c431bef9e3e85f8f0cc
m0rn <morn88@gmail.com>
2021-09-28 21:06:08 +0400

    Arrow functions

--- app.js
+++ app.js
@@ -1,57 +1,30 @@
-function getThis() {
-    console.log(this);
+const plus = (x = 0, y = 0) => x + y;
+const plusRes = plus(1, 2);
+console.log(plusRes);
+
+
+const withoutArgs = () => console.log("Hello world");
+const singleArg = x => x * 2;
+
+console.log(plus());
+
+
+const moreActions = (a, b) => {
+    a *= 2;
+    b *= 3;
+    return a + b;
 }
 
-// getThis();
+const returnObj = (str = '') => ({
+    value: str,
+    length: str.length,
+})
 
-function getPrice() {
-    return this;
+const sum = (...props) => {
+    const params = Array.prototype.slice.call(props);
+    if (!params.length) return 0;
+    return params.reduce((prev, next) => prev + next);
 }
 
-function getName() {
-    return this;
-}
-
-const prod1 = {
-    name: `Intel`,
-    price: 100,
-    getPrice,
-    getName: function () {
-        return this.name;
-    },
-};
-
-prod1.getPrice();
-
-const prod2 = {
-    name: `AMD`,
-    price: 50,
-    getPrice,
-};
-
-console.log(prod1.getPrice());
-console.log(prod2.getPrice());
-console.log(prod1.getName());
-
-prod2.getName = prod1.getName;
-console.log(prod2.getName());
-
-
-let str = 'Hello world';
-
-const reversStr = str.split('').reverse().join('');
-console.log(reversStr);
-
-const prod3 = {
-    name: 'ARM',
-    price: 200,
-    getPrice,
-    getName,
-};
-
-
-const getPriceBind = prod3.getPrice.bind(prod3);
-
-console.log(getPriceBind);
-
-setTimeout(getPriceBind, 1000);
+console.log(sum(1, 2, 3, 4));
+console.log(sum());
