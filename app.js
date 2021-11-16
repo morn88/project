78e59aa5c3eb840d9f4c306106a7f710530ac6b8
m0rn <morn88@gmail.com>
2021-11-16 23:11:55 +0400

    Destruct

--- app.js
+++ app.js
@@ -1,17 +1,72 @@
-let obj1 = {
-    name: 'Vasja',
+const user = {
+    firstName: 'Vasja',
+    lastName: 'Pupkin',
+    age: 25,
     info: {
+        work: 'Joppa',
         skills: ['html', 'css'],
     },
 };
 
-let obj2 = {
-    name: 'Ivan',
-    age: 20,
+// const firstName = user.firstName;
+// const lastName = user.lastName;
+
+// const { firstName: name, lastName, age: years = 30 } = user;
+// const {
+//   info: { work, skills },
+// } = user;
+// console.log(skills);
+
+const colors = ['white', 'black', 'red', '#89432d'];
+const [w, b, red, green = 'green'] = colors;
+// console.log(w, b, red, green);
+const names = ['Denis', 'Ivan', 'Maksim'];
+const [name1, ...otherNames] = names;
+console.log(name1, otherNames);
+const nestedArr = ['hello world', ['key', 'value']];
+const [, [key, value]] = nestedArr;
+// console.log(key, value);
+// const [...newNames] = names;
+const newNames = [...names];
+// console.log(...newNames);
+const colorNames = ['some value', ...colors, ...names];
+// console.log(colorNames);
+const newUser = {
+    ...user,
+    age: 30
 };
+console.log(newUser);
+const {
+    info: {
+        skills: [html, css],
+    },
+} = newUser;
 
-let newObj = Object.assign({}, obj2, obj1);
-newObj = Object.assign({}, obj1);
-//console.log(newObj);
+// console.log(html, css);
 
-newObj = JSON.parse(JSON.stringify(obj1));
+function myPerson({
+    lastName = '&',
+    firstName = '&',
+    info: {
+        skills
+    } = {},
+} = {}) {
+    console.log(skills);
+}
+
+// myPerson(newUser);
+function foo(x, y, ...others) {
+    console.log(arguments);
+    // console.log(Array.prototype.slice.call(arguments));
+    const [...args] = arguments;
+    console.log(others);
+}
+
+// foo(1, 2, 5, 6);
+const numbers = [2, 3];
+
+function foo2(x, y) {
+    console.log(x, y);
+}
+
+foo2(...numbers);
