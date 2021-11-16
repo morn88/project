fe95e889619d41c7f8c05f675c077939d06602d9
m0rn <morn88@gmail.com>
2021-11-16 23:19:45 +0400

    advanced object descriptor

--- app.js
+++ app.js
@@ -1,72 +1,32 @@
-const user = {
-    firstName: 'Vasja',
-    lastName: 'Pupkin',
-    age: 25,
-    info: {
-        work: 'Joppa',
-        skills: ['html', 'css'],
+const car = {
+    brand: 'Audi',
+    year: 2019,
+    get age() {
+        return `Мышина выпущена в ${this.year}`;
+    },
+    set age(value) {
+        this.year = value;
     },
 };
 
-// const firstName = user.firstName;
-// const lastName = user.lastName;
+// Object.defineProperty(car, 'age', {
+//   // configurable: false,
+//   // enumerable: false,
+//   // writable: false,
+//   get: function() {
+//     return `Мышина выпущена в ${this.year}`;
+//   },
+// });
 
-// const { firstName: name, lastName, age: years = 30 } = user;
-// const {
-//   info: { work, skills },
-// } = user;
-// console.log(skills);
-
-const colors = ['white', 'black', 'red', '#89432d'];
-const [w, b, red, green = 'green'] = colors;
-// console.log(w, b, red, green);
-const names = ['Denis', 'Ivan', 'Maksim'];
-const [name1, ...otherNames] = names;
-console.log(name1, otherNames);
-const nestedArr = ['hello world', ['key', 'value']];
-const [, [key, value]] = nestedArr;
-// console.log(key, value);
-// const [...newNames] = names;
-const newNames = [...names];
-// console.log(...newNames);
-const colorNames = ['some value', ...colors, ...names];
-// console.log(colorNames);
-const newUser = {
-    ...user,
-    age: 30
-};
-console.log(newUser);
-const {
-    info: {
-        skills: [html, css],
+Object.defineProperty(window, 'globalVar', {
+    set: function (value) {
+        console.log(value);
+        debugger;
     },
-} = newUser;
+});
 
-// console.log(html, css);
-
-function myPerson({
-    lastName = '&',
-    firstName = '&',
-    info: {
-        skills
-    } = {},
-} = {}) {
-    console.log(skills);
+function foo() {
+    globalVar = 10;
 }
 
-// myPerson(newUser);
-function foo(x, y, ...others) {
-    console.log(arguments);
-    // console.log(Array.prototype.slice.call(arguments));
-    const [...args] = arguments;
-    console.log(others);
-}
-
-// foo(1, 2, 5, 6);
-const numbers = [2, 3];
-
-function foo2(x, y) {
-    console.log(x, y);
-}
-
-foo2(...numbers);
+foo();
