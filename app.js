879bdbe5aa22285af5bc137a6b0d8668242509b3
m0rn <morn88@gmail.com>
2021-11-16 22:59:43 +0400

    Copying objects

--- app.js
+++ app.js
@@ -1,57 +1,17 @@
-function getFullName(firstName, lastName) {
-    return function () {
-        return `$(firstName) $(lastName)`;
-    };
-}
+let obj1 = {
+    name: 'Vasja',
+    info: {
+        skills: ['html', 'css'],
+    },
+};
 
-const getName = getFullName('Petja', 'Pupkin');
-//console.log(getName);
+let obj2 = {
+    name: 'Ivan',
+    age: 20,
+};
 
-function updateVal(val = 0) {
-    let x = val;
-    return function (num = 0) {
-        return x += num;
-    };
-}
+let newObj = Object.assign({}, obj2, obj1);
+newObj = Object.assign({}, obj1);
+//console.log(newObj);
 
-const updtVal = updateVal(2);
-const updtVal2 = updateVal(4);
-// console.log(updtVal(1));
-// console.log(updtVal(0));
-// console.log(updtVal2(2));
-// console.log(updtVal2(0));
-
-
-function checkCred() {
-    const login = 'test';
-    const password = 'somepassword';
-
-    return {
-        checkLogin(value) {
-            return login === value;
-        },
-        checkPassword(value) {
-            return password === value;
-        },
-    };
-}
-
-const check = checkCred();
-// console.log(check);
-// console.log(check.checkLogin('asdasdsd'));
-
-function closureExample() {
-    const arrOfFunc = [];
-    let value = '';
-    for (let i = 0; i < 10; i++) {
-        value += i;
-        arrOfFunc.push(function () {
-            console.log(value, i);
-        });
-    }
-    return arrOfFunc;
-}
-
-const res = closureExample();
-res[0]();
-res[5]();
+newObj = JSON.parse(JSON.stringify(obj1));
